441c08fb83a721dea5914e5c5004aad1ae12970e
Wenchy C. Dutreuil <wcdutreuil@gmail.com>
2021-10-18 22:47:58 -0500

    Working on styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,16 @@ import {Col, Container, Row} from "react-bootstrap";
 
 const Footer = () => {
     return (
-        <Container fluid className="bg-light fixed-bottom">
-            <Row className="">
+        <>
+            <div className="mt-lg-5"></div>
+        <Container fluid className="bg-light fixed-bottom mt-lg">
+            <Row>
                 <Col className={"text-center"}>
                     All rights reserved Wenchy Tech LLC &copy; 2021
                 </Col>
             </Row>
         </Container>
+            </>
     );
 }
 
--- src/components/house_view.tsx
+++ src/components/house_view.tsx
@@ -9,42 +9,37 @@ const HouseView = () => {
         <>
             <NavBar/>
             <Container>
-                {
-                    (() => {
-                        const rows = [];
-                        for (var i = 0; i < 3; i++) rows.push(
-                            <Row className="my-5">
-                                <CardGroup>
-                                    {
-                                        (() => {
-                                            const cols = [];
-                                            for (var i = 0; i < 5; i++) cols.push(
-                                                <Col>
-                                                    <Card style={{width: '15rem'}}>
-                                                        <Card.Img variant="top" src="Capture0.png"/>
-                                                        <Card.Body>
-                                                            <Card.Title>House {house++ + 1}</Card.Title>
-                                                            <Card.Text>
-                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                                                Maxime mollitia,
-                                                                molestiae quas vel sint commodi repudiandae consequuntur
-                                                                voluptatum laborum
-                                                            </Card.Text>
-                                                            <Card.Link href="#">Link A</Card.Link>
-                                                            <Card.Link href="#">Link B</Card.Link>
-                                                        </Card.Body>
-                                                    </Card>
-                                                </Col>
-                                            );
-                                            return cols;
-                                        })()
-                                    }
-                                </CardGroup>
-                            </Row>
-                        );
-                        return rows;
-                    })()
-                }
+                <CardGroup>
+                    <Row xs={1} sm={2} md={3} lg={4} xl={5} className="my-5 align-content-center">
+
+                        {
+                            (() => {
+                                const cols = [];
+                                for (var i = 0; i < 15; i++) cols.push(
+                                    <Col>
+                                        <Card style={{width: '15rem'}}>
+                                            <Card.Img variant="top" src="Capture0.png"/>
+                                            <Card.Body>
+                                                <Card.Title>House {house++ + 1}</Card.Title>
+                                                <Card.Text>
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                    Maxime mollitia,
+                                                    molestiae quas vel sint commodi repudiandae consequuntur
+                                                    voluptatum laborum
+                                                </Card.Text>
+                                                <Card.Link href="#">Link A</Card.Link>
+                                                <Card.Link href="#">Link B</Card.Link>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                );
+                                return cols;
+                            })()
+                        }
+
+
+                    </Row>
+                </CardGroup>
             </Container>
             <Footer/>
         </>
@@ -52,7 +47,6 @@ const HouseView = () => {
 }
 
 HouseView.propTypes =
-    {
-    }
+    {}
 
 export default HouseView;
